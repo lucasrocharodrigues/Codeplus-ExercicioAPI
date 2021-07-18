6fc9476103c590ad0488e308ecfcb28e6d030ffa
lucasrocharodrigues <lucasrocha10111997@gmail.com>
2021-07-18 20:05:27 -0300

    ajuste api

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import api from './api.js'
 
+const config = {
+    method: 'GET',
+    url: 'products.json',
+    data: null
+}
+
+api(config, productList => {
+    console.log(productList)
+});
 
 //Neste exercício você deve imprimir na UL ".product-list" produtos seguindo o layout no README do gitbub: https://i.imgur.com/EbVlWpX.png
 //Deve incluir a soma dos produtos
